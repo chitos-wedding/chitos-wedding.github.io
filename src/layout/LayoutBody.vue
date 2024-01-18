9a869be1ffb85648c4996d3785d292e85a24a4d3
백서현 <hyunny0463@gmail.com>
2024-01-19 00:12:42 +0900

    test commit
    Signed-off-by: 백서현 <hyunny0463@gmail.com>

--- src/layout/LayoutBody.vue
+++ src/layout/LayoutBody.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="layout-body">
-    <!-- <TheGallery /> -->
+    <TheGallery />
     <!-- <TimelineItems /> -->
     <!-- <div class="title-wrapper"> -->
-    <q-btn @click="test">asdfasdf</q-btn>
     <div class="layout-body-title">
       <!-- <div class="title-item title-item-left">백서현</div> -->
       <div class="layout-body-title-item left">백서현</div>
@@ -15,6 +14,8 @@
     <MapInfo />
     <TheCalendar :year="2024" :month="4" :date="6" />
     <AccountInfo />
+    <GuestBook />
+    <DriveInfo />
     <!-- <div class="main-image-effect-wrapper">
       <img
         class="main-image-effect"
@@ -26,14 +27,11 @@
 
 <script setup>
 import { TheCalendar } from '@/components/calendar'
-import { AccountInfo, HallInfo, MapInfo } from '@/components/info'
+import { AccountInfo, DriveInfo, HallInfo, MapInfo } from '@/components/info'
 // import { TimelineItems } from '@/components/timeline'
-// import { TheGallery } from '@/components/gallery'
+import { TheGallery } from '@/components/gallery'
+import { GuestBook } from '@/components/guestbook'
 import MainImage from '@/components/MainImage.vue'
-
-const test = () => {
-  window.open('http://naver.me/xRPff5UI', '_blank')
-}
 </script>
 
 <style lang="scss" scoped>
@@ -41,6 +39,7 @@ const test = () => {
   max-width: 30rem;
   position: relative;
   background-color: #fdfdfd;
+  padding: 0 3rem;
 
   &-title {
     display: flex;
--- src/layout/LayoutHead.vue
+++ src/layout/LayoutHead.vue
@@ -1,5 +1,3 @@
 <template>
   <div class="layout-head"></div>
 </template>
-
-<script setup></script>
