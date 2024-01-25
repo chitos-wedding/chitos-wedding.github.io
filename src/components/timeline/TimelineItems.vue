9bc3670e5dc366e93d28bf160f39e3f2ee6955db
백서현 <hyunny0463@gmail.com>
2024-01-25 22:57:05 +0900

    modify timeline data
    Signed-off-by: 백서현 <hyunny0463@gmail.com>

--- src/components/timeline/TimelineItems.vue
+++ src/components/timeline/TimelineItems.vue
@@ -6,56 +6,38 @@
 <script setup>
 import { TimelineItem } from '.'
 
+const link = {
+  url: '',
+  text: '더보기 >',
+}
 const items = [
   {
+    link,
     text: '2019 첫 데이트\n가을의...',
     date: 'February 25 2019',
-    link: {
-      // url: 'https://github.com/florinpop17/app-ideas',
-      // text: 'Check it out on GitHub',
-      url: '',
-      text: '더보기 >',
-    },
+    // image: 'test.jpg',
   },
   {
+    link,
     text: '둘 만의 첫 프로젝트 ...',
     date: 'March 04 2019',
-    link: {
-      // url: 'https://florin-pop/blog/2019/03/weekly-coding-challenge/',
-      // text: 'Check it out here',
-      url: '',
-      text: '더보기 >',
-    },
+    image: 'main.jpg',
+    images: ['main.jpg', 'main.jpg', 'main.jpg'],
   },
   {
+    link,
     text: 'Got 1.000 followers on Twitter',
     date: 'March 07 2019',
-    link: {
-      // url: 'https://twitter.com/florinpop1705',
-      // text: 'See profile',
-      url: '',
-      text: '더보기 >',
-    },
   },
   {
+    link,
     text: 'I published my first article in the FreeCodeCamp Medium Publication',
     date: 'March 18 2019',
-    link: {
-      // url: 'https://medium.freecodecamp.org/how-to-build-a-double-slider-sign-in-and-sign-up-form-6a5d03612a34',
-      // text: 'Check it out here',
-      url: '',
-      text: '더보기 >',
-    },
   },
   {
+    link,
     text: 'Over 12.000 views in a single day on my Medium posts',
     date: 'April 05 2019',
-    link: {
-      // url: 'https://medium.com/@popflorin1705',
-      // text: 'See profile',
-      url: '',
-      text: '더보기 >',
-    },
   },
 ]
 </script>
