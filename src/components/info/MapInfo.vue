b4289a5aa4a298e5f9ebda6efcf2c23cea0a976e
백서현 <hyunny0463@gmail.com>
2024-01-28 23:57:31 +0900

    prevent map drag by lock
    Signed-off-by: 백서현 <hyunny0463@gmail.com>

--- src/components/info/MapInfo.vue
+++ src/components/info/MapInfo.vue
@@ -2,11 +2,15 @@
   <div class="map-info">
     <div class="title text-bold text-center">오시는 길</div>
     <div class="divider" />
+    <MapLink />
+    <KakaoMap />
   </div>
-  <MapLink />
-  <KakaoMap />
 </template>
 <script setup>
 import { KakaoMap, MapLink } from '@/components/map'
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.map-info {
+  padding: 3rem;
+}
+</style>
--- src/components/map/KakaoMap.vue
+++ src/components/map/KakaoMap.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="kakao-map">
+    <div class="kakao-map-icon-wrapper">
+      <q-icon
+        class="kakao-map-icon"
+        size="xs"
+        :name="isLocked ? 'lock' : 'lock_open'"
+        @click="isLocked = !isLocked"
+      />
+    </div>
     <div
       id="daumRoughmapContainer1705587233002"
       class="root_daum_roughmap root_daum_roughmap_landing"
-      style="width: 36rem"
-    />
+      style="width: auto; position: relative"
+    >
+      <div
+        :class="{ 'kakao-map-lock': isLocked }"
+        @mouseenter="isHovered = true"
+        @mouseleave="isHovered = false"
+      >
+        <div v-if="isLocked && isHovered" class="kakao-map-lock-dim">
+          <div class="kakao-map-lock-dim-message">
+            {{ '자물쇠 아이콘을 눌러\n터치 잠금해제 후 확대 및 이동해주세요.' }}
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+
+const isLocked = ref(true)
+const isHovered = ref(false)
+
 const executeScript = () => {
   const scriptTag = document.createElement('script')
   const inlineScript = document.createTextNode(`new daum.roughmap.Lander({
@@ -45,6 +70,45 @@ installScript()
 <style lang="scss" scoped>
 .kakao-map {
   display: flex;
+  flex-direction: column;
+  gap: 1rem;
   justify-content: center;
+  position: relative;
+
+  &-icon {
+    padding: 1rem;
+    border: 0.1rem solid $grey-4;
+    border-radius: 0.5rem;
+    background-color: white;
+    cursor: pointer;
+
+    &-wrapper {
+      display: flex;
+      align-items: center;
+    }
+  }
+
+  &-lock {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 21;
+
+    &-dim {
+      position: absolute;
+      top: 70%;
+      width: 100%;
+      height: 30%;
+      background-color: rgba($grey-9, 0.8);
+
+      &-message {
+        padding: 2rem;
+        white-space: pre-wrap;
+        color: white;
+        text-align: center;
+        line-height: 2rem;
+      }
+    }
+  }
 }
 </style>
