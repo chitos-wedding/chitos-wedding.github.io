333752eebd6598e1489aeb2f355fa8ec141fbffc
백서현 <hyunny0463@gmail.com>
2024-02-13 17:36:09 +0900

    참석 문구 '위해' 누락 추가
    Signed-off-by: 백서현 <hyunny0463@gmail.com>

--- src/components/info/CheckParticipation.vue
+++ src/components/info/CheckParticipation.vue
@@ -3,7 +3,7 @@
     <div class="title text-bold text-center">참석 정보</div>
     <div class="divider" />
     <div class="check-message">참석의 부담은 가지지 말아주시고</div>
-    <div class="check-message">정성껏 준비하기 여쭙는 것이니</div>
+    <div class="check-message">정성껏 준비하기 위해 여쭙는 것이니</div>
     <div class="check-message">참석 정보를 알려주시면 감사하겠습니다.</div>
     <div class="check-button style-button" @click="emit('open')">참석 정보 전달하기</div>
   </div>
